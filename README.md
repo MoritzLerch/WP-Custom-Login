70a9b48d5cb9b2df82645c6945bc757c309d6988
Moritz Lerch <moritz.ml.lerch@gmail.com>
2021-05-22 02:56:13 +0200

    changes and darkmode support

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# custom_login_logo
-With this Wordpress-Plugin you can replace the wordpress.org logo on the /wp-admin or /wp-login page to a custom logo to personalize the login interface appearance.
+# wp_custom_login
+With this Wordpress-Plugin you can replace the wordpress.org logo on the /wp-admin or /wp-login page to a custom logo to personalize the login interface appearance. A darkmode feature also is on board. 
 
-You only have to put your png named logo.png in a new folder named "img" parallel to all the themes you have installed in your webspace. You have to create it here: /wp-content/themes/img
+You only have to put your svg named `logo.svg` in the folder of this plugin and then in the subfolder `/assets`. You can also change the code for png's. 
 
-In the php file you have to put in your website url to get the function working. After packing it up in a .zip file and then uploading it to your WP-Plugins you have customized the wordpress.org logo with your own one. Nice, isn't it? ;-)
+After packing it up in a .zip file and then uploading it to your WP-Plugins you have customized the wordpress.org logo with your own one.
+
+Additional feauture: `/wp-admin` or `/wp-login` are now in darkmode. This is enabled by default
--- custom_login.php
+++ custom_login.php
@@ -2,22 +2,27 @@
   defined('ABSPATH') or die("Thanks for visting");
   /**
   * Plugin Name: Custom Login Page
-  * Plugin URI: https://moritz-lerch.de
-  * Description: Plugin, um das Anmeldefenster von WordPress zu personalisieren / momentan noch ohne Variablen
-  * Version: 1.7
+  * Plugin URI: https://www.moritz-lerch.de
+  * Description: Plugin, um das Anmeldefenster von WordPress zu personalisieren. Custom Logo + Darkmode
+  * Version: 1.8
   * Author: Moritz Lerch
-  * Author URI: https://moritz-lerch.de
+  * Author URI: https://www.moritz-lerch.de
   * License: GPL2
   * License URI:  https://www.gnu.org/licenses/gpl-2.0.html
   * Text Domain:  cus-wp-admin-logo
   */
 
-	
+
 // logo
-function custom_login_logo() { ?>
+function custom_login_logo() { 
+    $dir = plugin_dir_url( __FILE__ );
+    $reldir = str_replace(get_home_url(), "", $dir);
+    $file = $reldir . "assets/logo.svg";
+    
+    ?>
     <style type="text/css">
         body.login div#login h1 a {
-            background-image: url(http://YOURWEBSITEURL/wp-content/themes/img/logo.png);
+            background-image: url(<?php echo $file ?>);
 			background-size: 150px;
 			width: 150px;
 			height: 150px;
@@ -26,19 +31,51 @@ function custom_login_logo() { ?>
     </style>
 <?php }
 
-add_action( 'login_enqueue_scripts', 'custom_login_logo' );
+function custom_login_darkmode() { ?>
+    <style type="text/css">
+        body.login {
+            background-color: #141b2d;
+            color: #fff;
+        }
+        body.login > div > form {
+            background: #1f2940;
+            border: none; 
+            border-radius: 5px;
+        }
+        .login .button {
+            transition: ease-out 300ms;
+        }
+        .login #login_error, .login .message, .login .success {
+            background-color: #1f2940 !important;
+        }
+        .login form .input, .login form input[type="checkbox"], .login input[type="text"] {
+            background-color: #141b2d !important;
+        }
+        .login input[type="color"], input[type="date"], input[type="datetime-local"], input[type="datetime"], input[type="email"], input[type="month"], input[type="number"], input[type="password"], input[type="search"], input[type="tel"], input[type="text"], input[type="time"], input[type="url"], input[type="week"], select, textarea {
+            color: #fff !important;
+        }
+        .login #backtoblog a, .login #nav a {
+            color: #656d75 !important;
+            transition: ease-out 300ms;
+        }
+        .login #backtoblog a:hover, .login #nav a:hover, .login h1 a:hover {
+            color: #8da6b9 !important;
+        }
+    </style>
+<?php }
 
 // link
 function custom_login_logo_url() {
     return get_bloginfo( 'url' );
 }
 
-add_filter( 'login_headerurl', 'custom_login_logo_url' );
-
 // title
 function custom_login_logo_url_title() {
-    return get_bloginfo( 'blogname' );
+    return get_bloginfo('blogname');
 }
 
-add_filter( 'login_headertitle', 'custom_login_logo_url_title' );
+add_action('login_enqueue_scripts', 'custom_login_logo');
+add_action('login_enqueue_scripts', 'custom_login_darkmode');
+add_filter('login_headerurl', 'custom_login_logo_url');
+add_filter('login_headertitle', 'custom_login_logo_url_title');
 ?>
